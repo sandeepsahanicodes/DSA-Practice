93d1b1079e7da269ad472a61fe70797b3e0a6544
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2023-03-15 22:12:29 +0530

    Sample problem

--- src/main/java/com/week1/recursion/Num.java
+++ src/main/java/com/week1/recursion/Num.java
@@ -2,6 +2,19 @@ class Num
 {
     public static void main(String[] args) 
     {
-        System.out.println("");
+        System.out.println(sum(3));
+
+    }
+
+    public static int sum(int num)
+    {
+       if(num == 1)
+       {
+           return 1;
+       }
+       else
+       {
+           return sum(num-1);
+       }
     }
 }
