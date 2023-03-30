888198bd6dd298745878979125adc5e49b5c5866
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2023-03-30 22:45:32 +0530

    Added reverse number

--- src/main/java/com/week1/recursion/problems/easy/reverseNumber.java
+++ src/main/java/com/week1/recursion/problems/easy/reverseNumber.java
@@ -1,3 +1,14 @@
-public class reverseNumber {
-    
+public class reverseNumber 
+{
+    public static void main(String[] args) 
+    {
+        int number = 1024;
+        int rev = reverse(number);
+        System.out.println(rev);
+    }
+
+    public static reverse(int number)
+    {
+
+    }
 }
