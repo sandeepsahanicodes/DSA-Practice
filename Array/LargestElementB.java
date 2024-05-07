02177cac040a4d5095576370488056523104c0a6
Sandeep Sahani <sandeep.sahani@megabitsystems.com>
2024-05-07 12:10:14 +0530

    Find largest element in array: brute force approach

---
+++ Array/LargestElementB.java
@@ -0,0 +1,21 @@
+
+// Brute Force Approach
+/**
+ * 
+ * Step 1: Sort the array in assending
+ * Step 2: largest = arr[n-1]
+ * 
+ * Time Complexity: O(nlogn)
+*/
+
+import java.util.Arrays;
+class LargestElementB {
+    static int findLargestElement(int arr[]) {
+        Arrays.sort(arr);
+        return arr[arr.length-1];
+    }
+    public static void main(String[] args) {
+        int arr[] = {6,5,7,4,2,6,7,90};
+        System.out.println(findLargestElement(arr));
+    }
+}
