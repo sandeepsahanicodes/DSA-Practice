8ac1588e5b72798a323f6aac284489f829641103
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2024-12-22 17:17:03 +0530

    Add 4 pattern programs

--- Patterns/Patterns.java
+++ Patterns/Patterns.java
@@ -277,6 +277,182 @@ public class Patterns {
                 System.out.println();
              }
         }
+        // A 
+        // B B 
+        // C C C 
+        // D D D D 
+        // E E E E E 
+        public static void pattern16(int row) {
+            for (int i = 0; i < row; i++) {
+                char ch = (char)('A' + i);
+                for (int j = 0; j <= i; j++) {
+                    System.out.print(ch+" ");
+                }
+                System.out.println();
+             }
+        }
+    //      A    
+    //     ABA   
+    //    ABCBA  
+    //   ABCDCBA 
+    //  ABCDEDCBA
+        public static void pattern17(int row) {
+            for (int i = 0; i < row; i++) {
+                int space = row - i - 1;
+                // Space
+                for (int j = 0; j < space; j++) {
+                    System.out.print(" ");
+                }
+                // Character accending
+                for (char cha = 'A'; cha <= 'A'+ i; cha++) {
+                    System.out.print(cha);
+                }
+                //character decending
+                for (char chd = (char)('A'+ i - 1); chd >= 'A'; chd--) {
+                    System.out.print(chd);
+                }
+                // Space
+                for (int j = 0; j < space; j++) {
+                    System.out.print(" ");
+                }
+                System.out.println();
+             }
+        }
+
+        public static void pattern18(int row) {
+            char initChar;
+            for (int i = 0; i < row; i++) {
+                initChar = (char)('E'-i);
+                for (char ch= initChar; ch <= 'E' ; ch++) {
+                    System.out.print(ch+" ");
+        
+                }
+                System.out.println();
+             }
+        }
+        // **********
+        // ****  ****
+        // ***    ***
+        // **      **
+        // *        *
+        // *        *
+        // **      **
+        // ***    ***
+        // ****  ****
+        // **********
+        // Works for even rows only.(Partially correct.)
+        public static void pattern19A(int row) {
+            int space,stars;
+            for (int i = 0; i < row; i++) {
+                
+                if(i<=(row/2)-1) {
+                    space = 2*i; 
+                    stars = (row/2) - i;               
+                } else {
+                    space = 2*(row - i) - 2;
+                    stars = i - (row/2) + 1;
+                }
+                
+                // Stars
+                for (int j = 0; j < stars ; j++) {
+                    System.out.print("*");
+                }
+                // Space
+                for (int j = 0; j < space; j++) {
+                    System.out.print(" ");
+                }
+                // Stars
+                for (int j = 0; j < stars ; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+             }
+            
+        }
+        // **********
+        // ****  ****
+        // ***    ***
+        // **      **
+        // *        *
+        // *        *
+        // **      **
+        // ***    ***
+        // ****  ****
+        // **********
+        // Works for even/odd rows.(Fully correct)
+        public static void pattern19B(int row) {
+            int initalSpace = 0;
+            for (int i = 0; i < row; i++) {
+                // Stars
+                for (int j = 0; j < row-i ; j++) {
+                    System.out.print("*");
+                }
+                // Space
+                for (int j = 0; j < initalSpace; j++) {
+                    System.out.print(" ");
+                }
+                // Stars
+                for (int j = 0; j < row-i ; j++) {
+                    System.out.print("*");
+                }
+                initalSpace += 2;
+                System.out.println();
+             }
+             initalSpace = 2*row - 2;
+             for (int i = 0; i < row; i++) {
+                // Stars
+                for (int j = 0; j <= i ; j++) {
+                    System.out.print("*");
+                }
+                // Space
+                for (int j = 0; j < initalSpace; j++) {
+                    System.out.print(" ");
+                }
+                // Stars
+                for (int j = 0; j <= i ; j++) {
+                    System.out.print("*");
+                }
+                initalSpace -= 2;
+                System.out.println();
+             }
+        }
+        // *        *
+        // **      **
+        // ***    ***
+        // ****  ****
+        // **********
+        // ****  ****
+        // ***    ***
+        // **      ** 
+        // *        *
+        public static void pattern20(int row) {
+            int space,stars;
+            space = 2*row-2;
+            for (int i = 1; i <= 2*row-1; i++) {
+                stars = i;
+                if(i > row) {
+                    stars = 2*row - i;        
+                }
+                // Stars
+                for (int j = 1; j <= stars ; j++) {
+                    System.out.print("*");
+                }
+                // Space
+                for (int j = 1; j <= space; j++) {
+                    System.out.print(" ");
+                }
+                // Stars
+                for (int j = 1; j <= stars ; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+                if (i < row) {
+                    space -= 2;
+                } else {
+                    space += 2;
+                }
+             }
+        }
        
     public static void main(String[] args) {
         int t, row;
@@ -286,7 +462,7 @@ public class Patterns {
         for (int i = 0; i < t; i++) {
             System.out.print("Enter row for test case" + (i + 1) + ":");
             row = sc.nextInt();
-            pattern15(row);
+            pattern20(row);
         }
     }
 }
