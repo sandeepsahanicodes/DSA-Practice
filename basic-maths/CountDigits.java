0d1bdc4ec01044a3cab1108d8d44dca505c5f8c5
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2024-12-27 22:53:02 +0530

    Added basic math programs

---
+++ basic-maths/CountDigits.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CountDigits {
+    // Time complexity: O(log10(n))
+    public static int countDigits(int n) {
+        int count = 0;
+        while (n > 0) {
+            count++;
+            n = n / 10;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.println("Number of digits in " + n + " is: " + countDigits(n));
+    }
+}
---
+++ basic-maths/PalindromeNumber.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PalindromeNumber {
+
+    public static boolean isPalindrome(int n) {
+        if (n < 0) {
+            return false;
+        }
+        int reverse = 0;
+        int originalNumber = n;
+        while(n != 0) {
+            int lastDigit = n%10;
+            n=n/10;
+            reverse = (reverse*10) + lastDigit;
+        }
+        return originalNumber == reverse;
+    }
+
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        if (isPalindrome(n)) {
+            System.out.println("The number is a palindrome number");
+        } else {
+            System.out.println("The number is not a palindrome number");
+        }
+    }    
+}
