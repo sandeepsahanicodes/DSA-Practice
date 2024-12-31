d431f800ddf833ca846944d91f225b3c0be1bc29
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2024-12-31 17:43:47 +0530

    sum till n with non-parameterized method

--- recursion/SumTillN.java
+++ recursion/SumTillN.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class SumTillN {
+    // Parameterized method
     public static void sumTillN(int i, int sum) {
         if(i < 1) {
             System.out.println(sum);
@@ -8,11 +9,20 @@ public class SumTillN {
         }
         sumTillN(i-1, sum + i);
     }
+    
+    // Non-parameterized method
+    public static int sumTillN(int n) {
+        if(n == 0) {
+            return 0;
+        }
+        return n + sumTillN(n-1);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int n = sc.nextInt();
         sumTillN(n, 0);
         // System.out.println("Sum of numbers till " + n + " is: " + sumTillN(n, 0));
+        System.out.println("Sum of numbers till " + n + " is: " + sumTillN(n));
     }
 }
