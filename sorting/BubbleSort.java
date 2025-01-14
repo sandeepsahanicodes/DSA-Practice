b2119df7d06c83d34828f143b7555b8e43aa9935
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2025-01-14 14:54:49 +0530

    Optimized bubble sort.

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -5,13 +5,19 @@ public class BubbleSort {
     // TC: O(n) ~> Best
     public static void bubbleSort(int arr[], int n) {
         for (int i = n - 1; i >= 0; i--) {
+            boolean didSwap = false;
             for (int j = 0; j <= i - 1; j++) {
                 if (arr[j] > arr[j+1]) {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    didSwap = true;
                 }
             }
+            if (didSwap == false) {
+                break;
+            }
+            // System.out.println("Runs");
         }
     }
     public static void main(String[] args) {
